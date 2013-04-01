089762f267c22288cfcd3135fad8ed871b4f6e50
dreamlx <dreamlinx@gmail.com>
2013-04-01 23:50:04 +0800

    fix upload file name and language switch

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# coding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
 
@@ -60,5 +60,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  def filename
+     "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
 
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
