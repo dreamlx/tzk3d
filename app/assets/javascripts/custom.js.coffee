6f07781f53eec27ea798084e8b2ad4138ae1a98f
dreamlx <dreamlinx@gmail.com>
2013-04-14 21:38:37 +0800

    added dropdown

---
+++ app/assets/javascripts/custom.js.coffee
@@ -0,0 +1,2 @@
+$(document).ready ->
+  $('.dropdown-toggle').dropdown()
--- spec/models/remove_rating_from_product_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe RemoveRatingFromProduct do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
