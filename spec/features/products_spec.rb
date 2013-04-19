97012c7f1eb5c1e4a7e7bb12e525726d8fef991a
wyh770406 <wyh770406@gmail.com>
2013-04-19 22:00:54 +0800

    use relative path instead of full path

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,15 +2,24 @@
 feature "Products" do
   background do
     User.create!(:email => 'user@example.com', :password => 'caplin1234', :password_confirmation => 'caplin1234')
+    sign_in('user@example.com','caplin1234')
+  end
+
+  describe "GET /products", :type => :feature do
+    given(:file){File.new("#{Rails.root}/spec/files/resume.stl", 'rb')}
+    it "show currently created product" do
+      Product.create!(:name=>"test model",:model3d=>file)
+      visit products_path
+      page.should have_content("test model")
+    end
   end
 
   describe "POST /products GET /products", :type => :feature do
     it "creates product" do
-      sign_in('user@example.com','caplin1234')
       click_link "上传模型"
       visit new_product_path
       fill_in "product_name", :with => "mow lawn"
-      attach_file('product_model3d', '/home/wyh77/下载/resume.stl')
+      attach_file('product_model3d', "#{Rails.root}/spec/files/resume.stl")
       click_button "新增模型"
       # save_and_open_page
       page.should have_content("Product was successfully created.")
