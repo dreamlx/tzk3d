1a214683c0762b3e1f337ef611ec4cb6a0e5f8e2
dreamlx <dreamlinx@gmail.com>
2013-04-26 11:43:25 +0800

    hotfix product edit bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.order("created_at desc").paginate(page: params[:page], per_page: 21)
+     @products = Product.order("created_at desc").paginate(page: params[:page], per_page: 40)
     @categorys = Category.all
   end
 end
