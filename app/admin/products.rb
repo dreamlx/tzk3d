388e4f41840edffbe4d3f1113f7565edbb725d48
dreamlx <dreamlinx@gmail.com>
2013-05-09 16:12:44 +0800

    add product admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,26 +1,6 @@
 ActiveAdmin.register Product do
 
   show do 
-    h3 product.name
-    div do
-      product.designer
-    end
-    div do
-      product.price
-    end
-    div do
-      product.updated_at
-    end
-    div do 
-      product.desc
-    end
-    div do
-      product.status
-    end
-    product.pics.each do |pic|
-      div do
-        image_tag pic.photo.thumbnail.url
-      end
-    end
+    render "show"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
     if @product
       dscp = "产品说明: " + @product.desc unless @product.desc.nil?
     else
-      dscp =  "3d图纸库(3dtzk.com),是中国最大的3d打印机图纸设计、分享、下载平台，包括艺术品，首饰品，小工具，各种玩具、游戏、动漫等人物模型的3d设计图纸。"
+      dscp =  "3d图纸库(3dtzk.com),3d打印机图纸设计、分享、下载平台，包括艺术品，首饰品，小工具，各种玩具、游戏、动漫等人物模型的3d设计图纸。"
     end
     
     return dscp
