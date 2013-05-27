e32b1b1f82083f474d8c86348893c2f05c75239f
dreamlx <dreamlinx@gmail.com>
2013-05-27 09:36:37 +0800

    add logo and browse track

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tzk3d::Application.config.secret_token = '5dd59c8d5137b5e200bf714663b18c04008c5df5faa9257573b9519d052fd3773d41036228a76de17df3f8bfbd09de660af6008fa13eff8103c09a75d2a79771'
+Tzk3d::Application.config.secret_token = '51d59c8d5137b5e200bf714663b18c04008c5df5faa9257573b9519d052fd3773d41036228a76de17df3f8bfbd09de660af6008fa13eff8103c09a75d2a79771'
